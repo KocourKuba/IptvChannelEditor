053e8a87189401b728baed072220143068aad2cd
Pavel <pavel.abakanov@outlook.com>
2021-09-01 17:26:29 +0200

    Fix regex for sharaclub m3u entry

--- uri_sharaclub.cpp
+++ uri_sharaclub.cpp
@@ -5,7 +5,7 @@ void uri_sharaclub::parse_uri(const std::string& url)
 {
 	// http://em.gazoni1.com:80/live/s.265258.1d25ecee4e77d0419432d2e38eb0ee525/pervyHD/video.m3u8
 
-	static std::regex re_url(R"(https?:\/\/([\w\.]+):\d+\/live\/([\w\.]+)\/(\w+)\/.*\.m3u8)");
+	static std::regex re_url(R"(https?:\/\/([\w\.]+):\d+\/live\/([\w\.]+)\/([\w-]+)\/.*\.m3u8)");
 	std::smatch m;
 	if (std::regex_match(url, m, re_url))
 	{
