95b69ce40600ccd1c2d469f4800858905863aabe
Pavel <pavel.abakanov@outlook.com>
2021-10-07 22:40:51 +0200

    Do not update VerGIT.h if build number not changed

--- UpdateVer.cmd
+++ UpdateVer.cmd
@@ -2,8 +2,19 @@
 setlocal
 set PATH=%PATH%;"C:\Program Files\Git\usr\bin\"
 
+findstr "#define BUILD" VerGIT.h >tmp
+set /p str=<tmp
+del tmp
+set t=%str%
+for /f "tokens=3*" %%a in ("%t%") do (
+   set curbuild=%%a
+)
+
 for /f "delims=" %%a in ('git log --oneline ^| find "" /v /c') do @set BUILD=%%a
+if %curbuild%==%BUILD% goto :EOF
+
 for /F "tokens=2-4 delims= " %%a in ('git log -1 --pretty^=format:%%cD') do (
-sed 's/\$WCREV\$/%BUILD%/g;s/\$WCDATE=%%B %%d, %%Y\$/%%a %%b %%c/g;s/\$WCYEAR=%%Y\$/%%c/g' .\VerGIT.in.h >.\VerGIT.h
+	echo set BUILD to %BUILD%
+	sed 's/\$WCREV\$/%BUILD%/g;s/\$WCDATE=%%B %%d, %%Y\$/%%a %%b %%c/g;s/\$WCYEAR=%%Y\$/%%c/g' .\VerGIT.in.h >.\VerGIT.h
 )
 endlocal
