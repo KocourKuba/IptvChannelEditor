7f63f08a0013068b78d7f71dc2379db446c952bf
Pavel <pavel.abakanov@outlook.com>
2023-05-06 00:34:00 +0200

    Update proxy script

--- dune_plugin/www/cgi-bin/update_proxy.sh
+++ dune_plugin/www/cgi-bin/update_proxy.sh
@@ -2,16 +2,16 @@
 CURL=""
 source /tmp/run/versions.txt
 if [ $platform_kind == android ]; then
-CURL="/firmware/bin/curl"
-elif (echo $platform_kind | grep -E -q "87.."); then
-CURL="../../bin/curl.87xx"
+CURL="$FS_PREFIX/firmware/bin/curl"
 elif (echo $platform_kind | grep -E -q "864."); then
 CURL="../../bin/curl.864x"
-elif (echo $platform_kind | grep -E -q "867."); then
-CURL="../../bin/curl.867x"
 elif (echo $platform_kind | grep -E -q "865."); then
 CURL="../../bin/curl.865x"
+elif (echo $platform_kind | grep -E -q "867."); then
+CURL="../../bin/curl.867x"
+elif (echo $platform_kind | grep -E -q "87.."); then
+CURL="../../bin/curl.87xx"
 fi
 
-$CURL -k -s -D - --location $QUERY_STRING --user-agent "DuneHD/1.0"
+$CURL -k -s -D - --location $QUERY_STRING --user-agent "DuneHD/1.0 (product_id: $product; firmware_version: $firmware_version)"
 exit;
