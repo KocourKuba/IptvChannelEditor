d067dc787cf2eec014e75b053e34b5c50835d7ea
Pavel <pavel.abakanov@outlook.com>
2024-05-12 14:53:01 +0200

    Deny send log from dune with old firmware

--- dune_plugin/starnet_entry_handler.php
+++ dune_plugin/starnet_entry_handler.php
@@ -5,6 +5,20 @@ require_once 'lib/user_input_handler_registry.php';
 class Starnet_Entry_Handler implements User_Input_Handler
 {
     const ID = 'entry';
+    const ACTION_PLUGIN_ENTRY = 'plugin_entry';
+    const ACTION_LAUNCH = 'launch';
+    const ACTION_LAUNCH_VOD = 'launch_vod';
+    const ACTION_AUTO_RESUME = 'auto_resume';
+    const ACTION_UPDATE_EPFS = 'update_epfs';
+    const ACTION_INSTALL = 'install';
+    const ACTION_UNINSTALL = 'uninstall';
+    const ACTION_UPDATE = 'update';
+    const ACTION_DO_CHANNELS_SETTINGS = 'do_channels_setup';
+    const ACTION_DO_PLUGIN_SETTINGS = 'do_setup';
+    const ACTION_DO_REBOOT = 'do_reboot';
+    const ACTION_DO_POWER_OFF = 'power_off';
+    const ACTION_DO_SEND_LOG = 'do_send_log';
+    const ACTION_DO_CLEAR_EPG = 'do_clear_epg';
 
     private $plugin;
 
@@ -35,59 +49,62 @@ class Starnet_Entry_Handler implements User_Input_Handler
         if (!isset($user_input->control_id))
             return null;
 
+        hd_debug_print("user input control: $user_input->control_id", true);
+
         switch ($user_input->control_id) {
-            case 'do_reboot':
-                hd_debug_print("do reboot");
+            case self::ACTION_DO_REBOOT:
                 return Action_Factory::restart(true);
 
-            case 'power_off':
-                hd_debug_print("do power off");
+            case self::ACTION_DO_POWER_OFF:
                 if (is_limited_apk()) {
                     return Action_Factory::show_title_dialog(TR::t('entry_not_available'));
                 }
                 return array(send_ir_code(GUI_EVENT_DISCRETE_POWER_OFF));
 
-            case 'do_setup':
-                hd_debug_print("do setup");
+            case self::ACTION_DO_PLUGIN_SETTINGS:
                 return Action_Factory::open_folder('setup', TR::t('entry_setup'));
 
-            case 'do_channels_setup':
-                hd_debug_print("do channels setup");
+            case self::ACTION_DO_CHANNELS_SETTINGS:
                 return Action_Factory::open_folder('channels_setup', TR::t('tv_screen_channels_setup'));
 
-            case 'do_send_log':
-                hd_debug_print("do_send_log");
-                $error_msg = '';
-                $msg = HD::send_log_to_developer($error_msg) ? TR::t('entry_log_sent') : TR::t('entry_log_not_sent') . " $error_msg";
-                return Action_Factory::show_title_dialog($msg);
+            case self::ACTION_DO_SEND_LOG:
+                if (is_newer_versions()) {
+                    $error_msg = '';
+                    $msg = HD::send_log_to_developer($error_msg) ? TR::t('entry_log_sent') : TR::t('entry_log_not_sent') . " $error_msg";
+                    return Action_Factory::show_title_dialog($msg);
+                }
 
-            case 'do_clear_epg':
+                return Action_Factory::show_title_dialog(TR::t('entry_log_not_sent_too_old'));
+
+            case self::ACTION_DO_CLEAR_EPG:
                 $this->plugin->get_epg_manager()->clear_all_epg_cache();
                 $this->plugin->tv->unload_channels();
                 return Action_Factory::clear_rows_info_cache(Action_Factory::show_title_dialog(TR::t('entry_epg_cache_cleared')));
 
-            case 'plugin_entry':
+            case self::ACTION_PLUGIN_ENTRY:
                 if (!isset($user_input->action_id)) break;
 
                 hd_debug_print("plugin_entry $user_input->action_id");
-                $this->plugin->init_epg_manager();
+
+                if (!is_newer_versions()) {
+                    hd_debug_print("Too old Dune HD firmware! " . get_raw_firmware_version());
+                    return  Action_Factory::show_error(true, TR::t('err_too_old_player'),
+                        array(
+                            TR::load_string('err_too_old_player'),
+                            "Dune Product ID: " .get_product_id(),
+                            "Dune Firmware: " . get_raw_firmware_version(),
+                            "Dune Serial: " . get_serial_number(),
+                        ));
+                }
 
                 switch ($user_input->action_id) {
-                    case 'launch':
-                        if (!is_newer_versions()) {
-                            return  Action_Factory::show_error(true, TR::t('err_too_old_player'),
-                                array(
-                                    TR::load_string('err_too_old_player'),
-                                    "Dune Product ID: " .get_product_id(),
-                                    "Dune Firmware: " . get_raw_firmware_version(),
-                                    "Dune Serial: " . get_serial_number(),
-                                ));
-                        }
-
+                    case self::ACTION_LAUNCH:
                         if (toggle_https_proxy($this->plugin->get_bool_parameter(PARAM_USE_HTTPS_PROXY, false))) {
                             return Action_Factory::show_title_dialog(TR::t('entry_reboot_need'),
                                 Action_Factory::restart(), TR::t('entry_https_proxy_enabled'));
                         }
+
+                        $this->plugin->init_epg_manager();
 
                         if ((int)$user_input->mandatory_playback === 1
                             || (isset($plugin_cookies->auto_play) && $plugin_cookies->auto_play === SetupControlSwitchDefs::switch_on)) {
@@ -113,7 +130,9 @@ class Starnet_Entry_Handler implements User_Input_Handler
 
                         return $action;
 
-                    case 'auto_resume':
+                    case self::ACTION_AUTO_RESUME:
+                        $this->plugin->init_epg_manager();
+
                         if ((int)$user_input->mandatory_playback !== 1
                             || (isset($plugin_cookies->auto_resume) && $plugin_cookies->auto_resume === SetupControlSwitchDefs::switch_off)) break;
 
@@ -132,20 +151,22 @@ class Starnet_Entry_Handler implements User_Input_Handler
 
                         return Action_Factory::tv_play($media_url);
 
-                    case 'update_epfs':
-                        hd_debug_print("update_epfs");
-                        return Starnet_Epfs_Handler::update_all_epfs($plugin_cookies, isset($user_input->first_run_after_boot) || isset($user_input->restore_from_sleep));
-                    default:
+                    case self::ACTION_UPDATE_EPFS:
+                        $this->plugin->init_epg_manager();
+                        return Starnet_Epfs_Handler::update_all_epfs($plugin_cookies,
+                            isset($user_input->first_run_after_boot) || isset($user_input->restore_from_sleep));
+
+                    case self::ACTION_INSTALL:
+                    case self::ACTION_UPDATE:
+                        $this->plugin->upgrade_old_settings($plugin_cookies);
+                        toggle_https_proxy($this->plugin->get_bool_parameter(PARAM_USE_HTTPS_PROXY, false));
                         break;
 
-                    case 'uninstall':
+                    case self::ACTION_UNINSTALL:
                         $this->plugin->get_epg_manager()->clear_all_epg_cache();
                         break;
 
-                    case 'update':
-                    case 'install':
-                        $this->plugin->upgrade_old_settings($plugin_cookies);
-                        toggle_https_proxy($this->plugin->get_bool_parameter(PARAM_USE_HTTPS_PROXY, false));
+                    default:
                         break;
                 }
                 break;
