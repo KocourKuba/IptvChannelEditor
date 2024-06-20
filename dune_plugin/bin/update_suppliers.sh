db1fa2dcb3244781a252fc941a4dd77e7c85613a
Pavel <pavel.abakanov@outlook.com>
2024-06-20 09:38:58 +0200

    Sync scripts with ProIPTV

--- dune_plugin/bin/update_suppliers.sh
+++ dune_plugin/bin/update_suppliers.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-grep -q newgui2021 $FS_PREFIX/tmp/firmware_features.txt || exit 0
+grep -q newgui2021 "$FS_PREFIX/tmp/firmware_features.txt" || exit 0
 
-thisdir=`dirname $0`
-basedir=`cd "$thisdir/.." && pwd`
-plugin_name=`basename $basedir`
+thisdir=$(dirname "$0")
+basedir=$(cd "$thisdir/.." && pwd)
+plugin_name=$(basename "$basedir")
 
 dirpath="$FS_PREFIX/tmp/tv_app_suppliers"
-filepath="$dirpath/$plugin_name"
 
 [ -e "$dirpath" ] || mkdir "$dirpath"
 
