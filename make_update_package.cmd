18129d9f3db2d3e27d8842b0247c37deab294c8f
Pavel <pavel.abakanov@outlook.com>
2024-12-11 13:53:08 +0100

    externals

--- make_update_package.cmd
+++ make_update_package.cmd
@@ -162,7 +162,8 @@ dir /b /O:N *.exe *.dll *.pdb > upload.lst
 del /q *.exe *.dll *.pdb *.lst >nul 2>&1
 popd
 
-git.exe push --force --tags  -- "origin" master:master
+git tag %BUILD%
+git push --force --tags  -- "github" master:master
 
 echo done!
 goto :EOF
